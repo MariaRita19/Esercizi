5cbf7115de821e47d6eaf073f33ef72a4513dbd9
MariaRita19 <119576964+MariaRita19@users.noreply.github.com>
2023-02-20 22:04:51 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 
 cd Downloads # mi sposto in Downloads dove si trova la cartella scaricata "perAbInf.tgz" 
 tar xvzf perAbInf.tgz # estraggo la cartella "data" che contiene i tre set di misure a partire da "perAbInf.tgz"
-cp -r data /home/ubuntu/Documents/Progetto  # copio i tre set di misure in una nuova cartella "Progetto" specificandone il path
+cp -r data /home/ubuntu/Documents/Maria Rita/ProgettoAB_INF  # copio i tre set di misure in una nuova cartella "ProgettoAB_INF" specificandone il path
 exit
 
